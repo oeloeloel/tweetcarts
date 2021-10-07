c719a55e56c5df734224b65b97368d1b6d4aca79
oeloeloel <leo.prendergast@gmail.com>
2021-10-07 11:22:31 -0400

    Added Brightly tweetcart source

---
+++ brightly/main.rb
@@ -0,0 +1,9 @@
+TICK{|a|(X=rand 1280
+Y=rand 720
+A=rand(3)*r=90
+B=rand(r)+50
+C=[[q=220,r,r],[q,s=150,5],[r,r,q],[r,s,s]].sample)if(t=$t+=1)%270==1
+C.x+=u=t.sin
+C.y+=v=t.cos
+B.times{|n|sld!(X+(w=A+t).sin*n,Y+w.cos*n,(1+u)*30+5,(1+v)*30+5,C)}
+(h=a.outputs[:scene]).clear_before_render,h.w,h.h=p}
